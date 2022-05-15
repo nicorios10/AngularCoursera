b60866a66b4558626aeac1ce7f3da73696addcf3
nrios <nrios@baufest.com>
2022-05-15 18:09:14 -0300

    Data Binding

--- ConFusion/src/app/shared/dish.ts
+++ ConFusion/src/app/shared/dish.ts
@@ -1,11 +1,13 @@
-export class Dish{
-    id: string;
-    name: string;
-    image: string;
-    category: string;
-    label: string;
-    price: string;
-    description: string;
-    featured: boolean;
+import { Comment } from './comment';
 
+export class Dish {
+  id: string;
+  name: string;
+  image: string;
+  category: string;
+  featured: boolean;
+  label: string;
+  price: string;
+  description: string;
+  comments: Comment[];
 }
